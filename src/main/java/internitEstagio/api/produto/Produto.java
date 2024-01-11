bc1f0d368c45c30136b6367f0a301a7d1f26e41e
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-11 11:01:26 -0300

    feat: Criacao da JPA do produto

---
+++ src/main/java/internitEstagio/api/produto/Produto.java
@@ -0,0 +1,46 @@
+package internitEstagio.api.produto;
+
+import internitEstagio.api.categoria.Categoria;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "produtos")
+@Entity(name = "Produto")
+@EqualsAndHashCode(of = "id")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    private String nome;
+
+    private Long codigo;
+    private String status;
+    private Long quantidade;
+    private String descricao;
+    private String imagem;
+    private Boolean ativo;
+
+    public Produto(DadosCadastroProduto dados, Categoria categoria){
+        this.nome = dados.nome();
+        this.codigo = dados.codigo();
+        this.status = dados.status();
+        this.quantidade = dados.quantidade();
+        this.descricao = dados.descricao();
+        this.imagem = dados.imagem();
+        this.nome = dados.nome();
+        this.categoria = categoria;
+        this.ativo = true;
+    }
+
+}
