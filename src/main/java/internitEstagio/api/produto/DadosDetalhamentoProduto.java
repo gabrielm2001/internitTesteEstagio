3729c16afe8564f61eb109749f79aaa37eda8d08
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-11 11:01:05 -0300

    feat: Criacao do DTO de detalhamento de categoria

---
+++ src/main/java/internitEstagio/api/produto/DadosDetalhamentoProduto.java
@@ -0,0 +1,22 @@
+package internitEstagio.api.produto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosDetalhamentoProduto(
+        @NotBlank
+        String nome,
+        @NotBlank
+        Long codigo,
+        @NotBlank
+        String status,
+        @NotBlank
+        Long quantidade,
+        @NotBlank
+        String descricao,
+        @NotBlank
+        String imagem
+) {
+    public DadosDetalhamentoProduto(Produto produto){
+        this(produto.getNome(), produto.getCodigo(), produto.getStatus(), produto.getQuantidade(), produto.getDescricao(), produto.getImagem());
+    }
+}
