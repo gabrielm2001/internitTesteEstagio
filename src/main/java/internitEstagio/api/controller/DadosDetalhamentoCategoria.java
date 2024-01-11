aa0db5950f1f83debece7008b1f3422fb7a8b81b
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2024-01-10 22:08:17 -0300

    feat: Criacao do DTO de detalhamento de categoria

---
+++ src/main/java/internitEstagio/api/controller/DadosDetalhamentoCategoria.java
@@ -0,0 +1,13 @@
+package internitEstagio.api.controller;
+
+import internitEstagio.api.categoria.Categoria;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosDetalhamentoCategoria(        @NotNull
+                                                 String nome,
+                                                 @NotNull
+                                                 Long codigo) {
+    public DadosDetalhamentoCategoria(Categoria categoria){
+        this(categoria.getNome(), categoria.getCodigo());
+    }
+}
